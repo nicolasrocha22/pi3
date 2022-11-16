d9cdd1b3bb372b9b3f9754ad3ca2218949ea2036
Nicolas Rocha <61709133+nicolasrocha22@users.noreply.github.com>
2022-11-15 23:53:41 -0300

    Add purchase page

--- main.py
+++ main.py
@@ -13,5 +13,15 @@ def product():
     return render_template("product.html")
 
 
+@app.route("/buy")
+def buy():
+    return render_template("buy.html")
+
+
+@app.route("/final")
+def final():
+    return render_template("final.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
